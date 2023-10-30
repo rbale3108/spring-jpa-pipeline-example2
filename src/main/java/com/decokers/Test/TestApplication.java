242e4af47bb00d2235b616875dccdab5887d9354
rbale3108 <rbale0831@eng.rizvi.edu.in>
2023-10-30 16:10:53 +0530

    build step added in .yml file

--- src/main/java/com/decokers/Test/TestApplication.java
+++ src/main/java/com/decokers/Test/TestApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TestApplication.class, args);
+//		SpringApplication.run(TestApplication.class, args);
+		System.out.println("Hello world!, I'm Rohit Bale");
 	}
 
 }
